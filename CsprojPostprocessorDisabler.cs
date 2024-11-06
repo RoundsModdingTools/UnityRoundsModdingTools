83fd6dc506ad9fddfe78f45541d9013cf97c5a56
AALUND13 <tamlund1976@gmail.com>
2024-11-06 02:12:23 -0500

    Move `EditorPrefs.SetBool` to after `DisplayDialog` for `CsprojPostprocessorDisabler` prompt

--- CsprojPostprocessorDisabler.cs
+++ CsprojPostprocessorDisabler.cs
@@ -7,8 +7,6 @@ namespace UnityRoundsModdingTools.AssetPostprocessors {
     internal static class CsprojPostprocessorDisabler {
         static CsprojPostprocessorDisabler() {
             if(!EditorPrefs.GetBool("Original_CsprojPostprocessor_Detected_RunOnce", false) && File.Exists("Assets/Editor/CsprojPostprocessor.cs")) {
-                EditorPrefs.SetBool("Original_CsprojPostprocessor_Detected_RunOnce", true);
-
                 StringBuilder messageBuilder = new StringBuilder();
                 messageBuilder.AppendLine("Do you want to disable the original CsprojPostprocessor?");
                 messageBuilder.AppendLine();
@@ -20,6 +18,8 @@ namespace UnityRoundsModdingTools.AssetPostprocessors {
                     messageBuilder.ToString(),
                     "Yes", "No"
                 );
+
+                EditorPrefs.SetBool("Original_CsprojPostprocessor_Detected_RunOnce", true);
 
                 if(shouldDisableCsprojPostprocessor) {
                     File.Move("Assets/Editor/CsprojPostprocessor.cs", "Assets/Editor/CsprojPostprocessor.cs.disable");
