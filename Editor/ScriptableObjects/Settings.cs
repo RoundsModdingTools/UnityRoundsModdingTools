a8c23527c72ca394789c43ddaaa21aa985a62658
AALUND13 <tamlund1976@gmail.com>
2024-11-09 14:29:43 -0500

    Clean up `Settings` and change `Temp` path to `%Temp%/UnityRoundsModdingTools`

--- Editor/ScriptableObjects/Settings.cs
+++ Editor/ScriptableObjects/Settings.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace UnityRoundsModdingTools.Editor.ScriptableObjects {
     [CreateAssetMenu(fileName = "Settings", menuName = "Unity Rounds Modding Tools/Settings", order = 0)]
     internal class Settings : ScriptableSingleton<Settings> {
         [Header("Settings for Modding Tools")]
-        public string DllsFolderPath = @"Scripts/dlls";
-        public string ModsFolderPath = @"Scripts/Mods";
+        public string DllsFolderPath = "Scripts/dlls";
+        public string ModsFolderPath = "Scripts/Mods";
         public string BepinexAndHarmonyFolderPath = "Scripts/Mods/Bepinex and Harmony";
-        public string TempPath = @"C:/Temp";
+        public string TempPath = $"{Path.GetTempPath()}/UnityRoundsModdingTools";
 
         [Header("Settings for Coverting Project to Unity Project")]
-        public List<string> BlacklistedDirectory = new List<string>() { "obj", "bin", ".git", ".vs", "Assemblies" };
-        public List<string> BlacklistedFileExtension = new List<string> { "csproj", "sln", "dll" };
-        public List<string> WhitelistedUnityReferences = new List<string>() { "Unity.TextMeshPro", "Unity.TextMeshPro.Editor", "Unity.TextMeshPro.Editor.Tests", "Unity.TextMeshPro.Tests" };
+        [HideInInspector] public List<string> BlacklistedDirectory = new List<string>() { "obj", "bin", ".git", ".vs", "Assemblies" };
+        [HideInInspector] public List<string> BlacklistedFileExtension = new List<string> { "csproj", "sln", "dll" };
+        [HideInInspector] public List<string> WhitelistedUnityReferences = new List<string>() { "Unity.TextMeshPro", "Unity.TextMeshPro.Editor", "Unity.TextMeshPro.Editor.Tests", "Unity.TextMeshPro.Tests" };
     }
 }
