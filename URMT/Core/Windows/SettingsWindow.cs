51e9e5ed9381c44cd4b4abaa0eaf5d313ee83ee6
AALUND13 <tamlund1976@gmail.com>
2025-02-22 21:35:49 -0500

    Add SettingsWindow for URMT settings management

---
+++ URMT/Core/Windows/SettingsWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using URMT.Core.Managers;
+
+namespace URMT.Core.Windows {
+    public class SettingsWindow : EditorWindow {
+        private static Vector2 scrollPosition;
+
+        [MenuItem("URMT/Settings")]
+        public static void ShowWindow() {
+            GetWindow<SettingsWindow>("URMT Settings");
+        }
+        private void OnGUI() {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(EditorGUIUtility.currentViewWidth), GUILayout.Height(Screen.height - 30));
+
+            SettingsManager.RenderSettings();
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
