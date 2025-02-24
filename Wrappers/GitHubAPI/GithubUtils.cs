1b3f471e28d209f9a26ec9c975a0b7f4af8a094c
AALUND13 <tamlund1976@gmail.com>
2025-02-24 03:13:42 -0500

    Add GithubUtils class for URL validation and owner/repo extraction

---
+++ Wrappers/GitHubAPI/GithubUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GitHubAPI {
+    public static class GithubUtils {
+        public static bool IsValidGithubUrl(string url) {
+            string pattern = @"^https://github\.com/([^/]+/[^/]+)$";
+            return Regex.IsMatch(url, pattern);
+        }
+
+        public static (string owner, string repo) ExtractOwnerAndRepo(string githubUrl) {
+            string[] repoParts = githubUrl.Replace("https://github.com/", "").Split('/');
+            return (repoParts[0], repoParts[1]);
+        }
+    }
+}
