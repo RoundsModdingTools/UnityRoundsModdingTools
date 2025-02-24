6859e865a6c0d22bd9177d923cb648d31838acc0
AALUND13 <tamlund1976@gmail.com>
2025-02-23 20:48:47 -0500

    Remove the `old` folder

--- Editor/Old/Wrappers/Thunderstore/API/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace UnityRoundsModdingTools.Editor.Thunderstore.API.Entities {
-    public struct Category {
-        [JsonProperty("name")] public string Name;
-        [JsonProperty("slug")] public string Slug;
-    }
-}
