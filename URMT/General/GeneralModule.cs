3792d44301ea9e4f27dfbbeb4de2dd16ac66b2dc
AALUND13 <tamlund1976@gmail.com>
2025-02-24 20:58:43 -0500

    Ficrf message handling in GeneralModule

--- URMT/General/GeneralModule.cs
+++ URMT/General/GeneralModule.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using URMT.Core.Managers;
 using URMT.Core.Modules;
 using URMT.Core.Settings;
@@ -14,9 +17,18 @@ namespace URMT.General {
         public void OnModuleLoad() {
             // These messages exist so you don't have to reference the `URMT.General` assembly in your own code
             MessageBus.RegisterMessage("AddModBundleMappings", args => {
-                ModBundleMapping[] modBundleMappings = args.Select(arg => {
-                    (string modName, string assetBundleName) = ((string, string))arg;
-                    return new ModBundleMapping(modName, assetBundleName);
+                if(args.Length == 0 || args[0] == null) {
+                    LoggerUtils.LogError("AddFolderMappings received no arguments!");
+                    return;
+                }
+
+                if(!(args[0] is IEnumerable<(string, string)> collection)) {
+                    LoggerUtils.LogError("AddFolderMappings received invalid argument type!");
+                    return;
+                }
+
+                ModBundleMapping[] modBundleMappings = collection.Select(arg => {
+                    return new ModBundleMapping(arg.Item1, arg.Item2);
                 }).ToArray();
 
                 // Only add mappings that don't already exist
@@ -26,43 +38,83 @@ namespace URMT.General {
                     .ToArray();
 
                 GeneralModuleSettings.Instance.ModBundleMappings.AddRange(modBundleMappings);
+                EditorUtility.SetDirty(GeneralModuleSettings.Instance);
 
                 LoggerUtils.Log($"Added {modBundleMappings.Length} mod bundle mappings");
             });
 
             MessageBus.RegisterMessage("AddFolderMappings", args => {
-                FolderMapping[] folderMappings = args.Select(arg => {
-                    (string assemblyName, string folderName) = ((string, string))arg;
-                    return new FolderMapping(assemblyName, folderName);
+                if(args.Length == 0 || args[0] == null) {
+                    LoggerUtils.LogError("AddFolderMappings received no arguments!");
+                    return;
+                }
+
+                if(!(args[0] is IEnumerable<(string, string)> collection)) {
+                    LoggerUtils.LogError("AddFolderMappings received invalid argument type!");
+                    return;
+                }
+
+                FolderMapping[] folderMappings = collection.Select(arg => {
+                    return new FolderMapping(arg.Item1, arg.Item2);
                 }).ToArray();
 
                 // Only add mappings that don't already exist
                 folderMappings = folderMappings
                     .Where(mapping => !GeneralModuleSettings.Instance.FolderMappings
-                        .Any(existingMapping => existingMapping.FolderName == mapping.FolderName))
+                        .Any(existingMapping => existingMapping.AssemblyName == mapping.AssemblyName))
                     .ToArray();
 
                 GeneralModuleSettings.Instance.FolderMappings.AddRange(folderMappings);
+                EditorUtility.SetDirty(GeneralModuleSettings.Instance);
 
                 LoggerUtils.Log($"Added {folderMappings.Length} folder mappings");
             });
 
             MessageBus.RegisterMessage("RemoveModBundleMappings", args => {
-                string[] modNames = args.Cast<string>().ToArray();
+                if(args.Length == 0 || args[0] == null) {
+                    LoggerUtils.LogError("RemoveModBundleMappings received no arguments!");
+                    return;
+                }
+
+                IEnumerable<string> collection;
+                if(args[0] is string singleString) {
+                    collection = new List<string> { singleString };
+                } else if(args[0] is IEnumerable<object> objectCollection) {
+                    collection = objectCollection.OfType<string>().ToList();
+                } else {
+                    LoggerUtils.LogError($"RemoveFolderMappings received invalid argument type: {args[0].GetType()}");
+                    return;
+                }
+
 
                 GeneralModuleSettings.Instance.ModBundleMappings
-                    .RemoveAll(mapping => modNames.Contains(mapping.ModName));
+                    .RemoveAll(mapping => collection.Contains(mapping.ModName));
+                EditorUtility.SetDirty(GeneralModuleSettings.Instance);
 
-                LoggerUtils.Log($"Removed {modNames.Length} mod bundle mappings");
+                LoggerUtils.Log($"Removed {collection.Count()} mod bundle mappings");
             });
 
             MessageBus.RegisterMessage("RemoveFolderMappings", args => {
-                string[] assemblyNames = args.Cast<string>().ToArray();
+                if(args.Length == 0 || args[0] == null) {
+                    LoggerUtils.LogError("RemoveFolderMappings received no arguments!");
+                    return;
+                }
+
+                IEnumerable<string> collection;
+                if(args[0] is string singleString) {
+                    collection = new List<string> { singleString };
+                } else if(args[0] is IEnumerable<object> objectCollection) {
+                    collection = objectCollection.OfType<string>().ToList();
+                } else {
+                    LoggerUtils.LogError($"RemoveFolderMappings received invalid argument type: {args[0].GetType()}");
+                    return;
+                }
 
                 GeneralModuleSettings.Instance.FolderMappings
-                    .RemoveAll(mapping => assemblyNames.Contains(mapping.AssemblyName));
-                
-                LoggerUtils.Log($"Removed {assemblyNames.Length} folder mappings");
+                    .RemoveAll(mapping => collection.Contains(mapping.AssemblyName));
+                EditorUtility.SetDirty(GeneralModuleSettings.Instance);
+
+                LoggerUtils.Log($"Removed {collection.Count()} folder mappings");
             });
 
             LoggerUtils.Log("General Module Loaded");
