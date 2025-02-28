7d986cc5e5de07931208cfedd5de40fc043a68b2
AALUND13 <tamlund1976@gmail.com>
2025-02-27 19:51:44 -0500

    Add utility module with copy and paste functionality for Unity assets

---
+++ URMT/Utility/CopySystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace URMT.Utility.Menus {
+    public static class CopySystem {
+        [MenuItem("Assets/Copy #z", false, 0)]
+        public static void Copy() {
+            CopyHandler.ObjectsToCopy = Selection.objects;
+        }
+
+        [MenuItem("Assets/Copy #z", true)]
+        private static bool ValidateCopy() {
+            return IsInProjectWindow();
+        }
+
+        [MenuItem("Assets/Paste #x", false, 1)]
+        public static void Paste() {
+            var selectionPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+            // Ensure we're dealing with a folder
+            if(!AssetDatabase.IsValidFolder(selectionPath)) {
+                selectionPath = Path.GetDirectoryName(selectionPath);
+            }
+
+            CopyHandler.Paste(selectionPath);
+        }
+
+        [MenuItem("Assets/Paste #x", true)]
+        private static bool ValidatePaste() {
+            return IsInProjectWindow() && Selection.activeObject != null;
+        }
+
+        private static bool IsInProjectWindow() {
+            return EditorWindow.focusedWindow != null &&
+                   EditorWindow.focusedWindow.GetType().Name == "ProjectBrowser";
+        }
+    }
+}
---
+++ URMT/Utility/UtilityModule.cs
@@ -0,0 +1,15 @@
+﻿using URMT.Core.Modules;
+using URMT.Core.Settings;
+using URMT.Core.Utils;
+
+namespace URMT.Utility {
+    [URMTModule("Utility", "com.aalund13.urmt.utility")]
+    [URMTModuleDependency("com.aalund13.urmt.core")]
+    public class UtilityModule : IModuleEntry {
+        public ISettingMenu[] SettingMenus => new ISettingMenu[0];
+
+        public void OnModuleLoad() {
+            LoggerUtils.Log("Utility module loaded");
+        }
+    }
+}
